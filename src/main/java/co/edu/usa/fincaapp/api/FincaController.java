d23db75d43c129ede0fc2312cdebd134290235dc
julianpizarro <julian.pizarro.mt@correo.usa.edu.co>
2021-10-11 20:17:56 -0500

    Se agrega la anotacin status

--- src/main/java/co/edu/usa/fincaapp/api/FincaController.java
+++ src/main/java/co/edu/usa/fincaapp/api/FincaController.java
@@ -3,29 +3,33 @@ package co.edu.usa.fincaapp.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.usa.fincaapp.entidades.Finca;
 import co.edu.usa.fincaapp.servicios.FincaServicio;
 
 @RestController
-@RequestMapping("/api/v1/finca")
+@RequestMapping("/api/Farm")
 @CrossOrigin(origins = "*")
 
 public class FincaController {
     @Autowired private FincaServicio fincaServicio;
 
     @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
     public List<Finca> getTodasFincas(){
         return fincaServicio.getFincas();
     }
 
     @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
     public Finca guardar(@RequestBody Finca finca){
         Finca f = fincaServicio.guardarFinca(finca);
         return f;
